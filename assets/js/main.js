513bd0709b4b42b651cb5b76c2548d4452be59e4
Christopher Silich <chrissilich@gmail.com>
2017-11-30 20:12:41 -0500

    exercise written

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,18 @@
 "use strict";
 
 console.log("Hello World from main.js!");
+
+// 0. Note: you will find countless ways to make a hamburger navigation work online.
+// We're going to do it with vanilla JS, and CSS classes. 
+
+// 1. make it so when the user clicks the hamburger menu icon, the navigation appears.
+// The UL in there is already hidden (display: none). Use the classList property.
+// You'll have to make another class in the CSS that represents the nav in its "open" state.
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+
+// 2. Using CSS, style the newly visible navigation to look nice.
+
+// 3. Make it so that when you click the hamburger again, the nav closes (reverts back to normal).
+
+// 4. Use similar logic to #1 to make the hamburger icon itself look different when its 
+// clicked, and revert when its clicked again.
